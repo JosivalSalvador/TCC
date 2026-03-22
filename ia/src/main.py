9bc55e5047f04bc50a09515c760e280fecb06a6a
Josival Salvador <josivaljunior087@gmail.com>
2026-03-22 14:33:46 -0400

    fix: integrando husky e lint-staged na ia

--- ia/src/main.py
+++ ia/src/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 # É este carinha aqui que o terminal estava procurando!
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "Hello from IA! Rodando no monorepo!"}
